6943e80d1ae996a5b0f593cf6a6d668ed7c5c173
chanchishing <chishing@netvigator.com>
2023-02-23 12:48:00 +0800

    Keep and  change counter statue using useState()

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -1,15 +1,17 @@
+import { useState } from 'react'
 import './Counter.css'
 
 export default function Counter (){
 
-    function incrementCounterFunction(){
-        console.log('increment clicked')
+    const [count,setCount]=useState(0);
 
+    function incrementCounterFunction(){
+        setCount(count+1);
     }
 
     return(
         <div className="Counter">
-            <span className="count">0</span>
+            <span className="count">{count}</span>
             <div>
                 <button className="counterButton" 
                         onClick={incrementCounterFunction}>
