a134a8553bf37c5127e34f91313196e75ffeb496
chanchishing <chishing@netvigator.com>
2023-02-24 13:36:29 +0800

    Initial commit of a overall counter instead of counter for each button

--- src/components/counter/Counter.jsx
+++ src/components/counter/Counter.jsx
@@ -3,31 +3,56 @@ import {PropTypes} from 'prop-types'
 import './Counter.css'
 
 
-Counter.propTypes ={
+CounterButton.propTypes ={
     by: PropTypes.number
 }
 
-Counter.defaultProps ={
+CounterButton.defaultProps ={
     by: 1
 }
 
+export default function Counter() {
 
-export default function Counter ({by}){
+    const [count,setCount]=useState(0);
+
+    function incrementCounterParentFunction(by){
+        setCount(count + by);
+   }
+
+   function decrementCounterParentFunction(by){
+    setCount(count - by);
+}
+
+    
+    return(
+        <>
+            <span className="totalCount">{count}</span>
+            <CounterButton incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+            <CounterButton by={2} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+            <CounterButton by={5} incrementMethod={incrementCounterParentFunction} decrementMethod={decrementCounterParentFunction}/>
+        </>
+    )
+   
+}
+
+
+function CounterButton ({by,incrementMethod,decrementMethod}){
 
     const [count,setCount]=useState(0);
 
     function incrementCounterFunction(){
         setCount(count + by);
+        incrementMethod(by);
     }
 
     function decrementCounterFunction(){
         setCount(count - by);
+        decrementMethod(by);
     }
 
     return(
         <div className="Counter">
-            <span className="count">{count}</span>
-            <div>
+             <div>
                 <button className="counterButton" 
                         onClick={incrementCounterFunction}>
                     +{by}
