91feba9266f65389e583f0f7b1c4717c8b556228
chanchishing <chishing@netvigator.com>
2023-03-06 17:19:47 +0800

    adding axios to make REST call

--- src/components/todo/WelcomeComponent.jsx
+++ src/components/todo/WelcomeComponent.jsx
@@ -1,4 +1,5 @@
 import { useParams, Link } from 'react-router-dom';
+import axios from 'axios';
 
 export default function WelcomeComponent() {
 
@@ -6,11 +7,32 @@ export default function WelcomeComponent() {
 
     // console.log(username);
 
+    function callHelloWorldRestApi(){
+        console.log("hello world");
+        axios.get('http://localhost:8080/hello-world')
+            .then((response)=>sucessfulResponse(response))
+            .catch((error)=>errorResponse(error))
+            .finally(()=>console.log('cleanup'));
+    }
+
+    function sucessfulResponse(response) {
+        console.log(response);
+    }
+
+    function errorResponse(error) {
+        console.log(error);
+    }
+
+
     return (
         <div className="Welcome">
             <h1>Welcome {username}</h1>
             <div>    
                 Your todos - <Link to="/todos">Click Here</Link>
+            </div>
+            <div>
+                <button className="btn btn-success m-5" onClick={callHelloWorldRestApi}>Call Hello World</button>
+
             </div>
         </div>
     )
