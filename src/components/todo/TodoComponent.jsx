599a4fe8edb8041a039508fe4ac080e0b7835bcb
shing <chishing@netvigator.com>
2023-03-09 20:44:14 +0800

    Initial commit for displaying to do description in the Todo Component when update button in ListTodo Component is clicked

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -1,9 +1,30 @@
+import { retrieveTodoApi } from "./api/TodoApiService"
+import { useAuth } from "./security/AuthContext";
+import { useParams } from 'react-router-dom';
+import { useEffect, useState } from "react";
+
 export default function TodoComponent() {
+
+    const authContext=useAuth();
+    const username=authContext.username;
+
+    const {id}=useParams();
+
+    const [description,setDescription]=useState('');
+
+    function retrieveTodo(){
+        retrieveTodoApi(username,id)
+            .then(response=>setDescription(response.data.description))
+            .catch(error=>console.log(error));
+    }
+
+    useEffect( ()=>retrieveTodo(),[id] );
+
     return (
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-
+                desctiption:{description}
             </div>
         </div>
     )
--- src/components/todo/api/TodoApiService.js
+++ src/components/todo/api/TodoApiService.js
@@ -13,3 +13,5 @@ const apiClient =axios.create (
 export const retrieveAllTodosForUserNameApi = ((username)=>apiClient.get(`/users/${username}/todos`));
 
 export const deleteTodoApi = ((username,id)=>apiClient.delete(`/users/${username}/todos/${id}`));
+
+export const retrieveTodoApi = ((username,id)=>apiClient.get(`/users/${username}/todos/${id}`));
