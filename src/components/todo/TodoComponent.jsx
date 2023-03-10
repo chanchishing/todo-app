b9294ce14795e37b01d7d767aa3c89d2b73478fd
chanchishing <chishing@netvigator.com>
2023-03-10 13:29:51 +0800

    Using Formik and Moment to create Todo HTML Form for the TodoComponent

--- src/components/todo/TodoComponent.jsx
+++ src/components/todo/TodoComponent.jsx
@@ -2,7 +2,7 @@ import { retrieveTodoApi } from "./api/TodoApiService"
 import { useAuth } from "./security/AuthContext";
 import { useParams } from 'react-router-dom';
 import { useEffect, useState } from "react";
-
+import {Field, Form, Formik} from "formik"
 export default function TodoComponent() {
 
     const authContext=useAuth();
@@ -11,11 +11,20 @@ export default function TodoComponent() {
     const {id}=useParams();
 
     const [description,setDescription]=useState('');
+    const [targetDate,setTargetDate]=useState('');
+    //const [todo,setTodo]=useState('');
 
     function retrieveTodo(){
         retrieveTodoApi(username,id)
-            .then(response=>setDescription(response.data.description))
+            .then(response=>{setDescription(response.data.description);
+                             setTargetDate(response.data.targetDate);
+                            })
+            // .then(response=>setTodo(response.data))
             .catch(error=>console.log(error));
+    }
+
+    function onSubmit(values) {
+        console.log(values);
     }
 
     useEffect( ()=>retrieveTodo(),[id] );
@@ -24,7 +33,28 @@ export default function TodoComponent() {
         <div className="container">
             <h1>Enter Todo Details</h1>
             <div>
-                desctiption:{description}
+                <Formik initialValues={ { description, targetDate } } enableReinitialize={true}
+                    onSubmit={onSubmit}
+                >
+                {
+                    (props) => (
+                        <Form>
+                            <fieldset className="form-group">
+                                <label>Description</label>
+                                <Field type="text" className="form-control" name="description"/>
+                            </fieldset>
+                            <fieldset className="form-group">
+                                <label>Target Date</label>
+                                <Field type="date" className="form-control" name="targetDate"/>
+                            </fieldset>
+                            <div>
+                                <button type="submit" className="btn btn-success m-5" >Save</button>
+                            </div>
+                        </Form>
+                        )
+
+                }
+                </Formik>
             </div>
         </div>
     )
